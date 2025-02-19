17d8eae9343df130ff8a6b390643be87431c4dcd
Badsha Sheih <badshasheikh361@gmail.com>
2025-02-19 22:55:04 +0600

    Add files via upload

---
+++ acill.java
@@ -0,0 +1,11 @@
+public class acill {
+    public static void main(String[] args)
+    {
+        char ch='a';
+        int ascii=ch;
+        int castAscii=(int) ch;
+        System.out.println ("the ascii value of"+ ch + " is: " + ascii);
+        System.out.println("the ascii value of " + ch + " is:" +castAscii);
+    }
+    
+}
