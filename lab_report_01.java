9331b0cdf78031029f4f2f45da0a9f56d2193d81
Badsha Sheih <badshasheikh361@gmail.com>
2025-02-19 22:57:36 +0600

    Add files via upload

---
+++ lab_report_01.java
@@ -0,0 +1,11 @@
+/* write a programmer to print integer number */
+public class lab_report_01 {
+    public static void main(String args[] )
+    {
+        int i;
+        for(i=1;i<=10;i++){
+        System.out.println("Enter the interger value:"+i);
+        }
+    }
+    
+}
