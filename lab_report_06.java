95b6e2c1dabbf6ef4a9e2fd6a1e7fea510bc2115
Badsha Sheih <badshasheikh361@gmail.com>
2025-02-19 23:00:45 +0600

    Add files via upload

---
+++ lab_report_06.java
@@ -0,0 +1,19 @@
+/*Write a java program to find out the largest number in an array */
+public class lab_report_06 {
+    public static void main(String args[])
+    {
+        int arr[]={12,70,60,5,7};
+        int max=arr[0];
+        for(int i=0;i<arr.length;i++)
+        {
+            if (max< arr[0]);
+            {
+                max=arr[1];
+
+            }
+        }
+        System.out.println(max);
+        
+    }
+    
+}
