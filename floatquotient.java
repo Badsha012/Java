03c779417186a933ab1c633aaf4f43d5a566ece1
Badsha Sheih <badshasheikh361@gmail.com>
2025-02-19 22:55:52 +0600

    Add files via upload

---
+++ floatquotient.java
@@ -0,0 +1,13 @@
+public class floatquotient {
+    public static void main(String[] args)
+    {
+        float dividend=97,divisor=12;
+        float quotient=dividend / divisor;
+        float remainder=dividend % divisor;
+        System.out.println("Quotient ="+quotient);
+        System.out.println("Remainder="+remainder);
+
+
+    }
+    
+}
