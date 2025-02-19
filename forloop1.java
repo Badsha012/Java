509fe13b2e2c5353cc65b88ee955201e4437494f
Badsha Sheih <badshasheikh361@gmail.com>
2025-02-19 22:56:45 +0600

    Add files via upload

---
+++ forloop1.java
@@ -0,0 +1,16 @@
+public class forloop1 {
+    public static void main (String args [])
+    {
+        int a,b;
+        for(a=1;a<=5;a++)
+        {
+            for(b=1;b<5;b++)
+            {
+                System.out.println("1");
+            }
+            System.out.println(" ");
+
+        }
+    }
+    
+}
