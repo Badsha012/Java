7540d57500cfaca431cfcb4eb790ae697a31553e
Badsha Sheih <badshasheikh361@gmail.com>
2025-02-19 22:56:14 +0600

    Add files via upload

---
+++ deburgging.java
@@ -0,0 +1,11 @@
+public class deburgging {
+    public static void main(String [] args)
+    {
+        int i=42+45-48-5;
+        int j=5+5-8+2;
+        int ans=i%j;
+        System .out.println("calulate the ans="+ans);
+
+    }
+    
+}
