d55c267424b7b12e9121a0c05ea14f7f6ed26bdb
Badsha Sheih <badshasheikh361@gmail.com>
2025-02-20 10:11:36 +0600

    Add files via upload

---
+++ lab_report_11.java
@@ -0,0 +1,16 @@
+/* Write a java program to print the multiplaction table */
+
+public class lab_report_11 {
+    public static void main(String args[])
+    {
+        int num=5;
+        
+        for(int i=1;i<=10;++i)
+        {
+          
+          System.out.printf("%d *%d=%d\n",num,i,num *  i);
+
+        }
+    }
+    
+}
