138333d37466fd3256de36d06162f51bc9ab5dda
Badsha Sheih <badshasheikh361@gmail.com>
2025-02-20 10:04:28 +0600

    Add files via upload

---
+++ asciivalueexample.java
@@ -0,0 +1,13 @@
+public class asciivalueexample {
+    public static void main(String[]args)
+    {
+        char ch1='a';
+        char ch2='b';
+        int asciivalue1=ch1;
+        int asciivalue2=ch2;
+        System.out.printf("the ascii value=%d\n",asciivalue1);
+        System.out.printf("the ascii value=%d\n",asciivalue2);
+
+    }
+    
+}
