54a0012a50293e6060afe80758566d65cea28610
Badsha Sheih <badshasheikh361@gmail.com>
2025-02-20 10:13:17 +0600

    Add files via upload

---
+++ matrix.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class matrix {
+    public static void main(String [] args)
+    {
+        
+        int i,j;
+        int []n=new int[3][3];
+
+       
+      
+        for(i=0;i<=2;i++)
+        {
+            for(j=0;j<=2;j++)
+            {
+              
+               
+
+            }
+        }
+        for(i=0;i<=2;i++)
+        {
+            for(j=0;j<=2;j++)
+            {
+                System.out.println("Number[i][j]+\t");
+
+            }
+            System.out.println();
+
+        }
+
+    }
+    
+}
