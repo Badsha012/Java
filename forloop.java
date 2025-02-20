e996808328d00ec8d2dddaae731d1753dd0638f5
Badsha Sheih <badshasheikh361@gmail.com>
2025-02-20 10:07:41 +0600

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,18 @@
+public class forloop {
+    public static void main (String args []){
+        System.out.println("the right angle triangle of is shown below:\n");
+        int p,q;
+        for(p=1;p<=5;p++){
+
+        for(q=1;q<=p;q++)
+        {
+            System.out.println("1");
+
+        }
+        System.out.println( " ");
+
+    }
+    }
+
+    
+}
