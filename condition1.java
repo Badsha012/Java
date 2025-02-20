e44f60d57ac928783a6bb5d52149d4c8d51c4142
Badsha Sheih <badshasheikh361@gmail.com>
2025-02-20 10:06:15 +0600

    Add files via upload

---
+++ condition1.java
@@ -0,0 +1,16 @@
+public class condition1 {
+    public static void main(String args[])
+    {
+        int i=10;
+        if(i>9)
+        {
+            System.out.println("correct value");
+
+        }
+        else{
+            System.out.println("incorrect value");
+
+        }
+    }
+    
+}
