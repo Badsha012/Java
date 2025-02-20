8881bc79b9d6c6f08815a11a9fe8886a518fbbe6
Badsha Sheih <badshasheikh361@gmail.com>
2025-02-20 10:15:35 +0600

    Add files via upload

---
+++ pratice4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class pratice4 {
+    public static void main(String agrs[])
+    {
+        int a;
+        float b;
+        Scanner sc=new Scanner (System.in);
+        System.out.println("you entered the string");
+        System .out. println("Enter a interger:");
+        a=sc.nextInt();
+        System.out.println("you entered the interger9="+a);
+         System .out. println("Enter a float:");
+        b=sc.nextFloat();
+        System.out.println("you entered the float="+b);
+        sc.close();
+
+    }
+    
+}
