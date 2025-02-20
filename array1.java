654913058017da53f46b628e398cc187e8154398
Badsha Sheih <badshasheikh361@gmail.com>
2025-02-20 09:45:15 +0600

    Add files via upload

---
+++ array1.java
@@ -0,0 +1,17 @@
+public class array1 {
+    public static void main(String [] args)
+    {
+        int a[];
+        a=new int[5];
+        a[0]=5;
+        a[1]=7;
+        a[2]=9;
+        a[3]=18;
+        a[4]=54;
+        for(int i=0;i<a.length;i++)
+        {
+            System.out.println(+a[i]);
+        }
+    }
+    
+}
